fc1f8864e92e2331399909298506f8690c7a9ed4
Dominik Piotrowicz <dom.piotrowicz@gmail.com>
2021-03-08 22:58:06 +0100

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
